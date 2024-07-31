567b85286a51e668c90a3b3d0c7ffe7f5efa7761
Rishabh <rishabhmalav9@gmail.com>
2024-07-31 15:19:11 +0530

    added signup and password encryption

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig')
+const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+// const {User} = require('./models/index');
+// const bcrypt = require('bcrypt');
 
 const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server Started at PORT:  ${PORT}`); 
